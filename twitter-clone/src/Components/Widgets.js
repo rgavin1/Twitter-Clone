fd4cf63087b3db3ecbe5b833884ac46bdf193397
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2021-03-13 09:23:53 -0500

    Added Twitter Embed

--- twitter-clone/src/Components/Widgets.js
+++ twitter-clone/src/Components/Widgets.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import '../Assets/styles/Widgets.css';
 import { TwitterTweetEmbed } from 'react-twitter-embed';
+
 const Widgets = () => {
     return  <div className="widgets">
-                
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
+                <TwitterTweetEmbed tweetId={'933354946111705097'} />
             </div>
 }
 
