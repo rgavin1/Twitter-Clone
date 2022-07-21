9ac1b74459ee818e537e79ee2cd2798a9010504e
Ramsey Gavin <44215916+rgavin1@users.noreply.github.com>
2022-07-20 22:14:58 -0700

    More changes

--- server/index.js
+++ server/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
 const mockdata = require('./mockdata.json');
 
+app.use(cors());
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to Twitter Clone API - built by Ramsey Gavin')
 })
 
-
-
-// Posts
+// Random Posts
 app.get('/tweets', (req, res) => {
     res.send(mockdata)
+})
+
+app.post('/tweets', (req, res) => {
+  console.log('req', req)
+  res.send(req.body)
 })
 
 app.listen(port, () => {
